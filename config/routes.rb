29445b8e720a5ca4ab573e4bd88f65768cfdb82a
Baz86 <tombale@Toms-MacBook.local>
2019-09-19 18:49:38 +0100

    set root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root to: "twitter_users#index"
+
   resources :twitter_users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
